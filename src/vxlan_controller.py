533b7cc6721e04d0112f5d90c7a00b1dd11240a8
Zefkilis2002 <k.zefkilis@studenti.unitn.it>
2025-02-02 17:31:11 +0100

    Organized project structure and added captures

---
+++ src/vxlan_controller.py
@@ -0,0 +1,63 @@
+# Importazioni necessarie da Ryu
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ipv4
+from ryu.lib.packet import udp
+
+# Controller Ryu per gestire il tunnel VXLAN
+class VXLANController(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(VXLANController, self).__init__(*args, **kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Aggiunta di una regola di default per il table-miss
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(
+            datapath=datapath, priority=priority, match=match, instructions=inst
+        )
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+
+        if eth.ethertype == 0x0800:  # IPv4
+            ip_pkt = pkt.get_protocol(ipv4.ipv4)
+            if ip_pkt.proto == 17:  # UDP
+                udp_pkt = pkt.get_protocol(udp.udp)
+                self.logger.info("VXLAN Packet: src_ip=%s dst_ip=%s src_port=%d dst_port=%d", 
+                                 ip_pkt.src, ip_pkt.dst, udp_pkt.src_port, udp_pkt.dst_port)
+
+        actions = [datapath.ofproto_parser.OFPActionOutput(datapath.ofproto.OFPP_FLOOD)]
+        out = datapath.ofproto_parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=msg.data
+        )
+        datapath.send_msg(out)
---
+++ src/vxlan_topology.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+# Importazioni necessarie da Mininet e Python
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import OVSKernelSwitch, RemoteController
+from mininet.cli import CLI
+from mininet.link import TCLink
+from mininet.log import setLogLevel
+import os
+
+# Definizione della topologia VXLAN
+class VXLANTopo(Topo):
+    def __init__(self):
+        super(VXLANTopo, self).__init__()
+
+        # Creazione degli switch
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+
+        # Creazione degli host
+        h1 = self.addHost('h1', ip='10.0.0.1/24')
+        h2 = self.addHost('h2', ip='10.0.0.2/24')
+
+        # Collegamento degli host agli switch
+        self.addLink(h1, s1)
+        self.addLink(h2, s2)
+        self.addLink(s1, s2)
+
+
+def configure_vxlan(net):
+    # Configure switch IPs
+    s1, s2 = net.get('s1', 's2')
+    
+    # Configure IP addresses for switches
+    s1.cmd('ifconfig s1-eth2 192.168.1.1/24')
+    s2.cmd('ifconfig s2-eth2 192.168.1.2/24')
+    
+    # Create VXLAN interfaces
+    s1.cmd('ovs-vsctl add-port s1 vxlan1 -- set interface vxlan1 type=vxlan options:remote_ip=192.168.1.2 options:local_ip=192.168.1.1 options:key=1000')
+    s2.cmd('ovs-vsctl add-port s2 vxlan2 -- set interface vxlan2 type=vxlan options:remote_ip=192.168.1.1 options:local_ip=192.168.1.2 options:key=1000')
+    
+    # Add flows to handle VXLAN traffic
+    s1.cmd('ovs-ofctl add-flow s1 "table=0,priority=100,in_port=1 actions=output:vxlan1"')
+    s1.cmd('ovs-ofctl add-flow s1 "table=0,priority=100,in_port=vxlan1 actions=output:1"')
+    
+    s2.cmd('ovs-ofctl add-flow s2 "table=0,priority=100,in_port=1 actions=output:vxlan2"')
+    s2.cmd('ovs-ofctl add-flow s2 "table=0,priority=100,in_port=vxlan2 actions=output:1"')
+
+def start_network():
+    topo = VXLANTopo()
+    net = Mininet(
+        topo=topo,
+        switch=OVSKernelSwitch,
+        controller=None,  # We'll use OpenFlow rules directly
+        autoSetMacs=True
+    )
+    
+    net.start()
+    
+    # Configure VXLAN
+    configure_vxlan(net)
+    
+    # Enable IP forwarding
+    os.system('sysctl -w net.ipv4.ip_forward=1')
+    
+    print("*** Network started successfully")
+    print("*** To test connectivity:")
+    print("*** h1 ping h2")
+    print("*** To check VXLAN encapsulation:")
+    print("*** tcpdump -i any -nn port 4789")
+    
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    start_network()
